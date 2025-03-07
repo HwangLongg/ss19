805179e58b230b0f16cd429d36ba80146ea2a63d
HwangLongg <longlongokok2@gmail.com>
2025-03-07 10:19:31 +0700

    Update ss19-05.js

---
+++ ss19-05.js
@@ -0,0 +1,7 @@
+let testString = "Hello World";
+
+      if (testString.includes(" ")) {
+        alert("chuoi co chua dau cach");
+      } else {
+        alert("chuoi khong co dau cach.");
+      }
